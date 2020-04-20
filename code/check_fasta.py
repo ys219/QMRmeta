ec9566cbbabc6690551df8873989932a2a317972
ys219 <ys219@ic.ac.uk>
2020-04-20 10:19:24 +0100

    added new usages;20Apr20

--- code/check_fasta.py
+++ code/check_fasta.py
@@ -2,7 +2,6 @@
 # # -*- coding: utf-8 -*-
 
 
-
 from Bio import SeqIO
 from Bio import BiopythonWarning
 from Bio.Seq import Seq
@@ -62,10 +61,10 @@ def stopcount(seq_record, table, frame = (1,2,3)):
 ## main ##
 
 # now open the file and do something :P
-def check_property():
+def check_property(input,stop_table):
     global uniqs
     global out_df
-    with open(args.input) as infasta : 
+    with open(input) as infasta : 
         uniqs = [] # to save row names
         freq_list = [] # to save frequency info
         len_list = []# to save length info
@@ -80,7 +79,7 @@ def check_property():
             uniqs.append(head.split(';')[0].replace('>',''))## split by ; symbol, keep the first part then remove the > symbol
             freq_list.append(freq_check(head))# add frequency info
             len_list.append(length_check(seq))# add length info
-            stop_codon = min(stopcount(SeqRecord(Seq(seq)),5, frame = (1,2,3))) # check stop codon and retain the minimum
+            stop_codon = min(stopcount(SeqRecord(Seq(seq)),stop_table, frame = (1,2,3))) # check stop codon and retain the minimum
             stop_list.append(stop_codon)# append stop codon info
             # step += 1 # progress bar
             # percen = step/size # progress bar
@@ -99,12 +98,12 @@ def check_property():
 
 
 # to check chimera
-def check_chimera():
+def check_chimera(input):
     global out_df
     print('#Using Vsearch Filtering Chimera#')
     #
     # now let's do the chimera filtering!
-    subprocess.Popen("vsearch --uchime3_denovo %s --chimeras %s_chimeras.fa"%(args.input,filename),shell = True).wait()# chimeras will be exported
+    subprocess.Popen("vsearch --uchime3_denovo %s --chimeras %s_chimeras.fa"%(input,filename),shell = True).wait()# chimeras will be exported
     chime_list = ['False']*len(uniqs)# make all the cell be 'false'
     print('#Start checking Chimeras output#')
     with open(filename+'_chimeras.fa') as chimes:
@@ -125,10 +124,10 @@ def check_chimera():
     chimes.close()
 
 
-def out():
+def out(output):
     # output export
     out_df.to_csv(filename+'_info.csv',index= False, header = True)  
-    print('done')
+    print('done with info table')
 
 
 if __name__ == "__main__":
@@ -138,9 +137,12 @@ if __name__ == "__main__":
     parser.add_argument("input", help = "input file path", metavar = "INPUT.fasta/INPUT.fa")
     # extract filename
     # translation table numer
-    parser.add_argument("-t","--table", help = "the number referring to the translation table to use which follows the NCBI numbering convention (https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi)", metavar = "TABLE")
+    parser.add_argument("-t","--table", help = "the number referring to the translation table to use which follows the NCBI numbering convention (https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi)", metavar = "TABLE_NUM")
     # output file name with default
     parser.add_argument("-o","--output", help = "output directory (default is current directory)", default = "./", metavar = "OUTPUTFILENAME")# output
+    # qulity filter parameter
+    parser.add_argument("-q","--quality_maxee", help = "customise --fastq_maxee option in quality filtering with vsearch)", default = "1", metavar = "NUM")# 
+    parser.add_argument("-l","--derep_label", help = "customise --relabel option in dereplication with vsearch)", default = "uniq", metavar = "string")
     args = parser.parse_args()
     filename = os.path.splitext(os.path.basename(args.input))[0]
     ## potentially can add switch to each function?
@@ -148,6 +150,31 @@ if __name__ == "__main__":
     if os.path.getsize(args.input) == 0:# in input have contents
         sys.exit("Error: input file is empty")
     # maybe add checker to table?
-    check_property()
-    check_chimera()
-    out()
+    #
+    # quality filter, and dereplication:
+    print("###filtering the quality of input file###")
+    subprocess.Popen("vsearch --fastx_filter %s --fastq_maxee %s --fastaout %s_qf.fasta"%(args.input,args.quality_maxee,filename),shell=True).wait()
+    #
+    # dereplication
+    print("###Doing dereplication ###")
+    subprocess.Popen("vsearch --derep_fulllength %s --output %s_dereped.fasta --sizeout --relabel %s"%(filename+"_qf.fasta",filename,args.derep_label),shell=True).wait()
+    #
+    # maping derep with raw
+    print('##mapping raw with derep##')
+    subprocess.Popen("vsearch --search_exact %s -db %s -otutabout %s" %(filename+"_qf.fasta", filename+"_dereped.fasta", args.output+filename+"_origin_map.tsv"), shell = True).wait()
+    print("done with info table")
+    #
+    check_property(filename+"_dereped.fasta",args.table)
+    # if (chimera switch)
+    check_chimera(filename+"_dereped.fasta")
+    out(args.output+"[OUT]"+filename+"_info.csv")
+    #
+    # OTU delimitation_before:
+    print("###Delimitation with data before  fiiltering###")
+    subprocess.Popen("vsearch --cluster_size %s --id %s --centroids %s --sizein --relabel %s"%(filename+"_dereped.fasta","0.97",filename+"_OTU_before.fasta","OTU"),shell=True).wait()
+    #
+    # OTU_before mapping
+    subprocess.Popen("vsearch --usearch_global %s -db %s -id %s -otutabout %s"%(filename+"_dereped.fasta",filename+"_OTU_before.fasta","0.97",filename+"_OTU_bef_map.tsv"),shell=True).wait()
+    # 
+    #OTU_after mapping
+    
