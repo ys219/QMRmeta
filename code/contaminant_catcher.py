d19da711cff222de68155705ac17b329485e1f59
ys219 <ys219@ic.ac.uk>
2020-08-05 17:02:05 +0100

    updated usage

--- code/contaminant_catcher.py
+++ code/contaminant_catcher.py
@@ -7,22 +7,20 @@ from Bio.SeqIO.FastaIO import SimpleFastaParser
 import pandas as pd
 # import subprocess
 
-
-
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description = "test code")
     # parser.add_argument("-db" ,"--blast_datdbase",help= "blast searching database directory",,metavar = '<directory>/nt' default= '~/db/blast-db-2019-10-21/nt')
-    parser.add_argument("input" , help = "input files")
-    parser.add_argument("-b" ,"--barcode", help= "input barcode file")
-    parser.add_argument("-a" ,"--cleaned_ASVs", help= "input cleaned ASV file")
-    parser.add_argument("-m" ,"--mb_dir", help= "input metabarcoding files directorya")
-    parser.add_argument("-o","--output", help = "output filename ", default = "[INFO]conta_score.csv", metavar = "OUTPUTFILENAME")# output
-    # parser.add_argument("")
+    parser.add_argument("input" , help = "input barcodes files dircetory")
+    parser.add_argument("-bs" ,"--bc_selected", help= "input NAPselected barcode files")
+    parser.add_argument("-fa" ,"--filtered_ASVs", help= "input filtered ASVs fasta file")
+    parser.add_argument("-mb" ,"--mb_dir", help= "input metabarcoding concatenated files")
+    parser.add_argument("-cs","--conta_score_out", help = "output filename for contaminant score in mb (with default) ", default = "[INFO]contaminant_score.csv", metavar = "SCOREFILENAME")# score output
+    parser.add_argument("-cr","--conta_reads_out", help = "output filename for bc contaminat reads that occur in mb (with default)", default = "[INFO]contaminant_reads.fasta", metavar="READFILENAME")    
     args = parser.parse_args()
 
     ## extract ref dict
     ref_dic = {}
-    with open (args.barcode) as ref:
+    with open (args.bc_selected) as ref:
         for head , seq in SimpleFastaParser(ref):
             ref_dic[head.strip()] = seq.strip()
     
@@ -31,9 +29,9 @@ if __name__=="__main__":
     conta_count_dic = {}
     for path, dirs ,f in os.walk(args.input):
         for ref_head in ref_dic.keys():
-            if ref_head.strip()+".fastq" in f:#if the file be found in input folder
+            if ref_head.strip()+".fasta" in f:#if the file be found in input folder
 
-                with open(path+ref_head.strip()+".fastq") as fq:# open it, then rocord from the start 
+                with open(path+ref_head.strip()+".fasta") as fq:# open it, then record from the start 
                     
                     tmp_seq_list = []#empty list to store conta seq
                     for head , seq in SimpleFastaParser(fq):
@@ -55,7 +53,7 @@ if __name__=="__main__":
     ## filter with cleaned ASVs:
     # first extract dict
     ASV_dic = {}
-    with open (args.cleaned_ASVs) as ASV_file:
+    with open (args.filtered_ASVs) as ASV_file:
         for head , seq in SimpleFastaParser(ASV_file):
             ASV_dic[head] = seq
     # then filter concat dict:
@@ -66,18 +64,19 @@ if __name__=="__main__":
                 conta_dic[key].remove(seq)
                 # rm += 1
         conta_count_dic[key] = len(values)
-
+    
     
     ## loop through all mb files
     score = {}
     for  path,dirs , files in os.walk(args.mb_dir):
         for f in files:
             score[f] = {}
+            
             tmp = {}
-            with open (path+f) as mb:#open each file
+            with open (path+f,'r') as mb:#open each mb file
                 for head, seq in SimpleFastaParser(mb):#extract a tmp dict
                     tmp[head] = seq
-
+                    
             for ref_key , ref_seq in ref_dic.items(): #loop through ref_dict
                 score[f][ref_key] = None #inf retaining score
                 if ref_seq in tmp.values():
@@ -85,9 +84,15 @@ if __name__=="__main__":
                     if ref_key in conta_dic.keys() and len(conta_dic[ref_key]) != 0 :#check if barcoding exist in current file
                         for con_seq in conta_dic[ref_key]:
                             if con_seq in tmp.values():
-                                tmp_score += 1
+                                tmp_score += 1 
                         tmp_score = tmp_score/ len(conta_dic[ref_key])
                     score[f][ref_key] = tmp_score
     
     out_df = pd.DataFrame(score).T
-    out_df.to_csv(args.output,index = True)
+    out_df.to_csv(args.conta_score_out,index = True)
+    with open("conta_reads_out", "w") as conta_reads:
+        for key, value in conta_dic.items():
+            conta_reads.write(k)
+            for reads in value:
+                conta_reads.write(value.index())
+                conta_reads.write(reads)
