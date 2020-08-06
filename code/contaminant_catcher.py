6d7f7bebc5a81e6962c20a76322d040a3d8624d6
ys219 <ys219@ic.ac.uk>
2020-08-06 23:31:10 +0100

    6Aug; changed input

--- code/contaminant_catcher.py
+++ code/contaminant_catcher.py
@@ -13,7 +13,8 @@ if __name__=="__main__":
     parser.add_argument("input" , help = "input barcodes files dircetory")
     parser.add_argument("-bs" ,"--bc_selected", help= "input NAPselected barcode files")
     parser.add_argument("-fa" ,"--filtered_ASVs", help= "input filtered ASVs fasta file")
-    parser.add_argument("-mb" ,"--mb_dir", help= "input metabarcoding concatenated files")
+    parser.add_argument("-mb" ,"--mb_file", help= "input metabarcoding concatenated files")
+    parser.add_argument("-mn" ,"--mb_file_names", help= "input metabarcoding concatenated files names")
     parser.add_argument("-cs","--conta_score_out", help = "output filename for contaminant score in mb (with default) ", default = "[INFO]contaminant_score.csv", metavar = "SCOREFILENAME")# score output
     parser.add_argument("-cr","--conta_reads_out", help = "output filename for bc contaminat reads that occur in mb (with default)", default = "[INFO]contaminant_reads.fasta", metavar="READFILENAME")    
     args = parser.parse_args()
@@ -25,12 +26,11 @@ if __name__=="__main__":
             ref_dic[head.strip()] = seq.strip()
     
     ##extract contamenant dict
-    conta_dic = {}
-    conta_count_dic = {}
+    conta_dic = {}# store contaminat sequences
+    conta_count_dic = {}# store scores
     for path, dirs ,f in os.walk(args.input):
         for ref_head in ref_dic.keys():
-            if ref_head.strip()+".fasta" in f:#if the file be found in input folder
-
+            if ref_head.strip()+".fasta" in f:#if the file be found in input folder 
                 with open(path+ref_head.strip()+".fasta") as fq:# open it, then record from the start 
                     
                     tmp_seq_list = []#empty list to store conta seq
@@ -52,47 +52,68 @@ if __name__=="__main__":
     
     ## filter with cleaned ASVs:
     # first extract dict
-    ASV_dic = {}
+    ASV_list = []
     with open (args.filtered_ASVs) as ASV_file:
         for head , seq in SimpleFastaParser(ASV_file):
-            ASV_dic[head] = seq
+            ASV_list.append(seq.rstrip())
+    
     # then filter concat dict:
     for key, values in conta_dic.items():
         # rm = 0 # record how many been removed
         for seq in values:
-            if seq not in ASV_dic.values():
+            if seq not in ASV_list:
                 conta_dic[key].remove(seq)
-                # rm += 1
         conta_count_dic[key] = len(values)
     
+    # extract mb file names
+    with open(args.mb_file_names,"r") as mb_names:
+        mb_names_list = mb_names.readlines()
+        mb_names_list = [s.rstrip() for s in mb_names_list]
     
     ## loop through all mb files
     score = {}
-    for  path,dirs , files in os.walk(args.mb_dir):
-        for f in files:
-            score[f] = {}
-            
-            tmp = {}
-            with open (path+f,'r') as mb:#open each mb file
-                for head, seq in SimpleFastaParser(mb):#extract a tmp dict
-                    tmp[head] = seq
-                    
-            for ref_key , ref_seq in ref_dic.items(): #loop through ref_dict
-                score[f][ref_key] = None #inf retaining score
-                if ref_seq in tmp.values():
+    with open(args.mb_file,"r") as input:
+        for mb_f in mb_names_list:
+            score[mb_f] = {}
+            mb_tmp = []
+            for head, seq in SimpleFastaParser(input) :
+                if re.search(mb_f,head):
+                    mb_tmp.append(seq)
+            for ref_key , ref_seq in ref_dic.items():
+                score[mb_f][ref_key] = None
+                if ref_seq in mb_tmp:
                     tmp_score = 0
-                    if ref_key in conta_dic.keys() and len(conta_dic[ref_key]) != 0 :#check if barcoding exist in current file
+                    if ref_seq in conta_dic.keys() and len(conta_dic[ref_key]) != 0 :#check if barcoding exist in contaminant dict and contaminat is not empty
                         for con_seq in conta_dic[ref_key]:
-                            if con_seq in tmp.values():
+                            if con_seq in mb_tmp:
                                 tmp_score += 1 
                         tmp_score = tmp_score/ len(conta_dic[ref_key])
-                    score[f][ref_key] = tmp_score
+                    score[mb_f][ref_key] = tmp_score
+    # score = {}
+    # for  path,dirs , files in os.walk(args.mb_dir):
+    #     for f in files:
+    #         score[f] = {}
+            
+    #         tmp = {}
+    #         with open (path+f,'r') as mb:#open each mb file
+    #             for head, seq in SimpleFastaParser(mb):#extract a tmp dict
+    #                 tmp[head] = seq
+                    
+    #         for ref_key , ref_seq in ref_dic.items(): #loop through ref_dict
+    #             score[f][ref_key] = None #inf retaining score
+    #             if ref_seq in tmp.values():
+    #                 tmp_score = 0
+    #                 if ref_key in conta_dic.keys() and len(conta_dic[ref_key]) != 0 :#check if barcoding exist in current file
+    #                     for con_seq in conta_dic[ref_key]:
+    #                         if con_seq in tmp.values():
+    #                             tmp_score += 1 
+    #                     tmp_score = tmp_score/ len(conta_dic[ref_key])
+    #                 score[f][ref_key] = tmp_score
     
     out_df = pd.DataFrame(score).T
     out_df.to_csv(args.conta_score_out,index = True)
-    with open("conta_reads_out", "w") as conta_reads:
+    with open(args.conta_reads_out, "w") as conta_reads:
         for key, value in conta_dic.items():
-            conta_reads.write(k)
             for reads in value:
-                conta_reads.write(value.index())
-                conta_reads.write(reads)
+                conta_reads.write(key+"_"+str(value.index(reads)+1)+"\n")
+                conta_reads.write(reads+"\n")
